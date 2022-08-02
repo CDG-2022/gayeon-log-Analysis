feb31c26318ecd84df02e1a1b71e640a69cf8b92
gayeonP <pagaye@naver.com>
2022-08-02 21:08:40 +0900

    오타수정 및 주석추가

--- src/main/java/com/cdg/LogParser.java
+++ src/main/java/com/cdg/LogParser.java
@@ -14,7 +14,7 @@ public class LogParser {
             if (splitLog == null) break;
             logMap.getStateCodeMap().put(splitLog[0], logMap.getStateCodeMap().getOrDefault(splitLog[0], 0) + 1);
             logMap.getApiServiceMap().put(StringUtils.substringBetween(splitLog[1], "search/", "?"), logMap.getApiServiceMap().getOrDefault(StringUtils.substringBetween(splitLog[1], "search/", "?"), 0) + 1);
-            logMap.getApiKeyMap().put(StringUtils.substringBetween(splitLog[1], "=", "&"), logMap.getApiKeyMap().getOrDefault(StringUtils.substringBetween(splitLog[1], "apikey=", "&"), 0) + 1);
+            logMap.getApiKeyMap().put(StringUtils.substringBetween(splitLog[1], "=", "&"), logMap.getApiKeyMap().getOrDefault(StringUtils.substringBetween(splitLog[1], "=", "&"), 0) + 1);
             logMap.getWebBrowserMap().put(splitLog[2], logMap.getWebBrowserMap().getOrDefault(splitLog[2], 0) + 1);
             logMap.getPeakTimeMap().put(splitLog[3].substring(0, splitLog[3].length()-3), logMap.getPeakTimeMap().getOrDefault(splitLog[3].substring(0, splitLog[3].length()-3), 0) + 1);
         }

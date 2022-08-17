e36b8283900fa844eb8c1a305a5baa34ff0aed4a
gayeonP <pagaye@naver.com>
2022-08-17 22:04:00 +0900

    테스트 코드 작성을 위한 오류 수정

--- src/main/java/com/cdg/AnalysisManager.java
+++ src/main/java/com/cdg/AnalysisManager.java
@@ -1,12 +1,11 @@
 package com.cdg;
 
-import java.io.File;
 import java.io.IOException;
 
 public class AnalysisManager {
     private LogMap logMap = new LogMap();
     private LogParser logParser = new LogParser();
-    private LogFileWriter logFileWriter = new LogFileWriter(new File());
+    private LogFileWriter logFileWriter = new LogFileWriter();
 
     public void analysisStart() throws IOException {
         logParser.logParse(logMap);
